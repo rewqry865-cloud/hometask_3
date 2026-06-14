e0c6790b7bf6bc0ba79c5c4bb01f1fcfb57c4b2f
Малиновская Екатерина <edmalinovskaya@edu.hse.ru>
2026-06-14 21:44:39 +0300

    в return поменяла названия колонок

--- generate.py
+++ generate.py
@@ -11,10 +11,10 @@ COLUMNS = ["product_id", "price", "rating", "category"]
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "product_id": random.randint(0, 100),
+        "price": round(random.uniform(1.5, 9.9), 2),
+        "rating": random.randint(0, 100),
+        "category": random.choice(["A", "B", "C"]),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
